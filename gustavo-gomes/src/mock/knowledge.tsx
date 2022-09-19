51484d70e277256ca9787c05902b79d5c1c80951
gustavogomesribeiro <gustavo_roox@hotmail.com>
2022-09-19 16:54:14 -0300

    created function to list knowledge

---
+++ gustavo-gomes/src/mock/knowledge.tsx
@@ -0,0 +1,115 @@
+const knowledge = [
+    {
+        id: 'react',
+        name:'React.js',
+        descriptions: [
+            {
+                desc_1: 'Conhecimento react hooks (useContext, useEffect, useState, useCallback, useMemo)',
+                desc_2: 'Experiência com material UI, styled-components',
+            }
+        ]
+    },
+    {
+        id: 'react-native',
+        name:'React Native',
+        descriptions: [
+            {
+                desc_1: 'Conhecimento react hooks (useContext, useEffect, useState, useCallback, useMemo)',
+                desc_2: 'Experiência com material UI, styled-components',
+                desc_3: 'Experiência com Expo, React Native CLI',
+            }
+        ]
+    },
+    {
+        id: 'node',
+        name:'Node.js',
+        descriptions: [
+            {
+                desc_1: '',
+                desc_2: '',
+                desc_3: '',
+            }
+        ]
+    },
+    {
+        id: 'c#',
+        name:'C#',
+        descriptions: [
+            {
+                desc_1: '',
+                desc_2: '',
+                desc_3: '',
+            }
+        ]
+    },
+    {
+        id: 'java',
+        name:'Java',
+        descriptions: [
+            {
+                desc_1: '',
+                desc_2: '',
+                desc_3: '',
+            }
+        ]
+    },
+    {
+        id: 'angular',
+        name:'Angular',
+        descriptions: [
+            {
+                desc_1: '',
+                desc_2: '',
+                desc_3: '',
+            }
+        ]
+    },
+    {
+        id: 'aws',
+        name:'AWS',
+        descriptions: [
+            {
+                desc_1: 'Ec2',
+                desc_2: 'S3',
+                desc_3: 'Lambdas',
+                desc_4: 'Route53',
+                desc_5: 'Cloud Front',
+            }
+        ]
+    },
+    {
+        id: 'azure',
+        name:'Azure',
+        descriptions: [
+            {
+                desc_1: '',
+                desc_2: '',
+                desc_3: '',
+            }
+        ]
+    },
+    {
+        id: 'teste',
+        name:'Teste unitário/e2e',
+        descriptions: [
+            {
+                desc_1: '',
+                desc_2: '',
+                desc_3: '',
+            }
+        ]
+    },
+    {
+        id: 'scrum',
+        name:'Scrum',
+        descriptions: [
+            {
+                desc_1: '',
+                desc_2: '',
+                desc_3: '',
+            }
+        ]
+    },
+]
+
+export default knowledge;
--- gustavo-gomes/src/pages/Partials/knowledge/index.tsx
+++ gustavo-gomes/src/pages/Partials/knowledge/index.tsx
@@ -1,5 +1,7 @@
-import react from 'react';
+import react, {useState} from 'react';
 import Titles from '../../../components/Title';
+import knowledge from '../../../mock/knowledge';
+import knowledges from '../../../mock/knowledge';
 
 import {
     Container,
@@ -9,7 +11,28 @@ import {
     Descriptions
 } from './styled';
 
+interface Knowledge {
+    id: string;
+    name: string;
+    descriptions: [
+        {
+            desc_1?: string,
+            desc_2?: string,
+            desc_3?: string,
+            desc_4?: string,
+            desc_5?: string,
+        }
+    ];
+}
+
 export default function Knowledge() {
+    const [ filteredKnowledge, setFilteredKnowledge ] = useState<Knowledge[]>([]);
+
+    const showDescription = (id) => {
+        const knowledgeId = knowledges.filter( i => i.id === id );
+        setFilteredKnowledge(knowledgeId);
+    }
+
     return (
         <Container id="knowledge">
             <AreaContent>
@@ -19,65 +42,37 @@ export default function Knowledge() {
 
                 <AreaButton>
                     <ul>
-                        <li>
-                            <button id="react" autoFocus onClick={() => {}}>React.js</button>
-                        </li>
-                        <li>
-                            <button id="react-native" onClick={() => {}}>React Native</button>
-                        </li>
-                        <li>
-                            <button id="node" onClick={() => {}}>Node.js</button>
-                        </li>
-                        <li>
-                            <button id="c#" onClick={() => {}}>C#</button>
-                        </li>
-                        <li>
-                            <button id="java" onClick={() => {}}>Java</button>
-                        </li>
-                        <li>
-                            <button id="angular" onClick={() => {}}>Angular</button>
-                        </li>
-                        <li>
-                            <button id="aws" onClick={() => {}}>Aws</button>
-                        </li>
-                        <li>
-                            <button id="azure" onClick={() => {}}>Azure</button>
-                        </li>
-                        <li>
-                            <button id="teste" onClick={() => {}}>Teste unitário/e2e</button>
-                        </li>
-                        <li>
-                            <button id="scrum" onClick={() => {}}>Scrum</button>
-                        </li>
+                        {knowledges.map(knowledge => {
+                            return (
+                                <li key={knowledge.id}>
+                                    <button id={knowledge.id} autoFocus onClick={() => showDescription(knowledge.id)}>{knowledge.name}</button>
+                                </li>
+                            )
+                        })}
                     </ul>
-                    {/* {nzn && (
-                        <Descriptions>
-                            <h3>Full Stack Developer - NZN</h3>
-                            <p>December 2020 - Present</p>
-                            <div>
-                                <ul>
-                                    <li>Criação de novos produtos para a principal vertical Tecmundo.</li>
-                                    <li>Monitorar e aprimorar a performance de projetos já entregues em produção.</li>
-                                    <li>Principal stack utilizada: C#.</li>
-                                    <li>Competências: C# · JavaScript · ReactJs · Windows Azure · Scrum · Node.js · MongoDB · MySQL · GitHub</li>
-                                </ul>
-                            </div>
-                        </Descriptions>
-                    )}
-
-                    {starpay && (
-                        <Descriptions>
-                            <h3>Front End Developer - Starpay</h3>
-                            <p>August 2019 - November 2020</p>
-                            <div>
-                                <ul>
-                                    <li>Desenvolvimento de novos portais, atualizações e correções dos mesmos, através das stacks, ReactJs, React Native, AngularJs, NodeJs além de utilizar banco de dados Oracle, Git(Versionamento de Código) e Aws.</li>
-                                    <li>Configuração de terminais POS e integração com sistemas externos via APIs RestFul e EDI com arquivos posicionais padrão CNAB.</li>
-                                    <li>Competências: React.js · Android · AngularJS · Java · JavaScript · Node.js · CSS · Aws · GitHub</li>
-                                </ul>
-                            </div>
-                        </Descriptions>
-                    )} */}
+                    
+                    {filteredKnowledge.map(knowledge => {
+                        return (
+                            <Descriptions key={knowledge.id}>
+                                <h3>{knowledge.name}</h3>
+                                <div>
+                                    <ul>
+                                        {knowledge.descriptions.map(description => {
+                                            return (
+                                                <div key={Math.random() * 2}>
+                                                    <li>{description?.desc_1}</li>
+                                                    <li>{description?.desc_2}</li>
+                                                    <li>{description?.desc_3}</li>
+                                                    <li>{description?.desc_4}</li>
+                                                    <li>{description?.desc_5}</li>
+                                                </div>
+                                            )
+                                        })}
+                                    </ul>                                    
+                                </div>
+                            </Descriptions>                            
+                        )
+                    })}
                 </AreaButton>
             </AreaContent>
         </Container>
